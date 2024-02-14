84b99d8043772bf9886b2c7f0f8b438e9fc93e90
Preetjot Dhami <preetjot02@gmail.com>
2024-02-13 22:40:53 -0800

    added launch file for urdf, updated urdf

--- src/ik_interface/launch/display.launch.py
+++ src/ik_interface/launch/display.launch.py
@@ -1,52 +1,40 @@
-import launch
-from launch.substitutions import Command, LaunchConfiguration
-import launch_ros
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 import os
 
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
 def generate_launch_description():
-    pkgPath = launch_ros.substitutions.FindPackageShare(package='ik_interface').find('ik_interface')
-    urdfModelPath= os.path.join(pkgPath, 'urdf/rover_arm.urdf')
-    
-    with open(urdfModelPath,'r') as infp:
-    	robot_desc = infp.read()
-    
-    params = {'robot_description': robot_desc}
-    
-    robot_state_publisher_node =launch_ros.actions.Node(
-    package='robot_state_publisher',
-	executable='robot_state_publisher',
-    output='screen',
-    parameters=[params])
-    
-    
-    joint_state_publisher_node = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        parameters=[params],
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
-    )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
-    )
-    
-    rviz_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen'
+
+    pkg_box_bot_gazebo = get_package_share_directory('box_bot_gazebo')
+
+    # Sart World
+    start_world = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_box_bot_gazebo, 'launch',
+                         'start_world_launch.py'),
+        )
     )
 
-    return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
-                                            description='This is a flag for joint_state_publisher_gui'),
-        launch.actions.DeclareLaunchArgument(name='model', default_value=urdfModelPath,
-                                            description='Path to the urdf model file'),
-        robot_state_publisher_node,
-        joint_state_publisher_node,
-        joint_state_publisher_gui_node,
-        rviz_node
-    ]) 
+    spawn_robot_world = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_box_bot_gazebo, 'launch',
+                         'spawn_robot_ros2.launch.py'),
+        )
+    )
+
+    start_rviz = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_box_bot_gazebo, 'launch', 'start_rviz.launch.py'),
+        )
+    )
+
+    return LaunchDescription([
+        start_world,
+        spawn_robot_world,
+        start_rviz
+    ])
