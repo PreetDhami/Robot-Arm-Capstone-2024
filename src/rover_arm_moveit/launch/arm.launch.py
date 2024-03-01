e1fc54b5e0d5e922092189b9ccb703b1966a1162
Preetjot Dhami <preetjot02@gmail.com>
2024-02-29 16:21:35 -0800

    replaced arm_teleop node with moveit_servo in launch

--- src/rover_arm_moveit/launch/arm.launch.py
+++ src/rover_arm_moveit/launch/arm.launch.py
@@ -11,17 +11,32 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import os
+import yaml
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, RegisterEventHandler
-from launch.conditions import IfCondition
+from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
-
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.actions import ComposableNodeContainer
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch_param_builder import ParameterBuilder
 from moveit_configs_utils import MoveItConfigsBuilder
+from launch_ros.descriptions import ComposableNode
+
+
+from ament_index_python.packages import get_package_share_directory
+
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, "r") as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
 
 def generate_launch_description():
     # Declare arguments
@@ -40,7 +55,13 @@ def generate_launch_description():
     )
     robot_description = {"robot_description": robot_description_content}
 
+    moveit_config = (
+        MoveItConfigsBuilder("rover_arm", package_name="rover_arm_moveit")
+        .robot_description(file_path="config/rover_arm.urdf.xacro")
+        .to_moveit_configs()
+    )
 
+    servo_params = {"moveit_servo": load_yaml("rover_arm_moveit", "config/servo_config.yaml")}
 
 
     robot_controllers = PathJoinSubstitution(
@@ -89,16 +110,6 @@ def generate_launch_description():
 
 
 
-    joy_node = Node(
-        package="joy",
-        executable="joy_node",
-    )
-
-    arm_teleop_node = Node(
-        package="arm_teleop",
-        executable="arm_teleop_node",
-    )
-
 
     # Delay start of robot_controller after `joint_state_broadcaster`
     delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
@@ -109,14 +120,74 @@ def generate_launch_description():
     )
 
 
+    container = ComposableNodeContainer(
+        name="moveit_servo_container",
+        namespace="/",
+        package="rclcpp_components",
+        executable="component_container_mt",
+        composable_node_descriptions=[
+            # Example of launching Servo as a node component
+            # Assuming ROS2 intraprocess communications works well, this is a more efficient way.
+            # ComposableNode(
+            #     package="moveit_servo",
+            #     plugin="moveit_servo::ServoServer",
+            #     name="servo_server",
+            #     parameters=[
+            #         servo_params,
+            #         moveit_config.robot_description,
+            #         moveit_config.robot_description_semantic,
+            #     ],
+            # ),
+            # ComposableNode(
+            #     package="robot_state_publisher",
+            #     plugin="robot_state_publisher::RobotStatePublisher",
+            #     name="robot_state_publisher",
+            #     parameters=[moveit_config.robot_description],
+            # ),
+            ComposableNode(
+                package="tf2_ros",
+                plugin="tf2_ros::StaticTransformBroadcasterNode",
+                name="static_tf2_broadcaster",
+                parameters=[{"child_frame_id": "/base_link", "frame_id": "/world"}],
+            ),
+            ComposableNode(
+                package="moveit_servo",
+                plugin="moveit_servo::JoyToServoPub",
+                name="controller_to_servo_node",
+            ),
+            ComposableNode(
+                package="joy",
+                plugin="joy::Joy",
+                name="joy_node",
+            ),
+        ],
+        output="screen",
+    )
+    # Launch a standalone Servo node.
+    # As opposed to a node component, this may be necessary (for example) if Servo is running on a different PC
+    servo_node = Node(
+        package="moveit_servo",
+        executable="servo_node_main",
+        parameters=[
+            servo_params,
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+        ],
+        output="screen",
+    )
+
+
+
+
 
     nodes = [
         control_node,
         robot_state_pub_node,
         joint_state_broadcaster_spawner,
         delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
-        joy_node,
-        arm_teleop_node,
+        servo_node,
+        container,
 
     ]
 
