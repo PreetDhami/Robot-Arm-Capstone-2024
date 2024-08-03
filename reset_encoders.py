878d23a845e17e78e36f5e8384d9d67bafb191dc
PreetDhami <108783637+PreetDhami@users.noreply.github.com>
2024-08-03 14:43:51 -0700

    asdf

--- reset_encoders.py
+++ reset_encoders.py
@@ -2,21 +2,24 @@ import can
 import time
 
 
-filters = [
-    {"can_id": 0x009, "can_mask": 0x01f, "extended": False},
-]
-bus = can.interface.Bus(channel="can0", interface="socketcan", can_filters=filters)
-time.sleep(0.5)
 
-ids = {}
 
-for msg in bus:
-    ids[int(msg.arbitration_id)] = msg.data
+
+
+def main(): 
+    filters = [
+        {"can_id": 0x009, "can_mask": 0x01f, "extended": False},
+    ]
+    bus = can.interface.Bus(channel="can0", interface="socketcan", can_filters=filters)
+    time.sleep(0.5)
+
+    ids = {}
+
+    for msg in bus:
+        ids[int(msg.arbitration_id)] = msg.data
     print(ids)
 
 
 
-
-
-
-
+if __name__=="__main__": 
+    main() 
